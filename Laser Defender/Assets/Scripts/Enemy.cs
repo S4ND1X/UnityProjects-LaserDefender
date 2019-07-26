82ca59d60720d79e97ebe1c50dfc0344a5a0b63e
S4ND1X <42609763+S4ND1X@users.noreply.github.com>
2019-07-26 17:56:18 -0500

    Enemy Shooting

--- Laser Defender/Assets/Scripts/Enemy.cs
+++ Laser Defender/Assets/Scripts/Enemy.cs
@@ -6,16 +6,40 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] private float healt = 100;
+    [SerializeField] private float shotCounter;
+    [SerializeField] private float minTimeBetweenShots = 0.2f;
+    [SerializeField] private float maxTimeBetweenShots = 3f;
+    [SerializeField] private float enemyLaserSpeed = 10f;
+
+    [SerializeField] private GameObject enemyLaserPrefab;
     // Start is called before the first frame update
     void Start()
     {
-
+        shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
     }
 
     // Update is called once per frame
     void Update()
     {
+        CountDownAndShoot();
+    }
 
+    private void CountDownAndShoot()
+    {
+        shotCounter -= Time.deltaTime;
+        if (shotCounter <= 0f)
+        {
+            Fire();
+            shotCounter = Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+    {
+        GameObject enemyLaser = Instantiate(enemyLaserPrefab,
+                                             transform.position,
+                                             Quaternion.identity) as GameObject;
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -enemyLaserSpeed);
     }
 
     private void OnTriggerEnter2D(Collider2D other)
